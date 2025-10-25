df0aa823828ee8e52745c79599b62764db769eb8
vkarthikeyan12 <kar120997@gmail.com>
2025-10-25 18:46:10 +0530

    Add files via upload

---
+++ NotepadExtension/background.js
@@ -0,0 +1,7 @@
+chrome.action.onClicked.addListener(async (tab) => {
+  if (!tab.id) return;
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"]
+  });
+});
