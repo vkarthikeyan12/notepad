60ea13f545ade001ca81e09f0c53e733ea519074
vkarthikeyan12 <kar120997@gmail.com>
2025-10-25 18:46:39 +0530

    Delete NotepadExtension directory

--- NotepadExtension/background.js
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  if (!tab.id) return;
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
-});
